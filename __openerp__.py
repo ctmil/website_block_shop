1fc7f32621b62cd88b62c588a81f18d1176c0dac
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-15 08:10:39 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Website - Block shop access',
+    'category': 'Website',
+    'version': '0.1',
+    # 'depends': ['base','elmatica_sales_purchase','portal_purchase','elmatica_tickets'],
+    'depends': ['base','website_sale'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    # 'css': ['static/src/css/styles.css',],
+    'installable': True,
+}
